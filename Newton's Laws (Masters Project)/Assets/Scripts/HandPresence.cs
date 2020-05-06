443b7a566b7b9b12e01e7e746b46fb3512557092
FluxOblivion <flux.oblivion@gmail.com>
2020-05-07 02:19:15 +0930

    XR rig working
    Hand control and interaction functioning; working on movement and UI next.

---
+++ Newton's Laws (Masters Project)/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+
+    InputDeviceCharacteristics leftControllerCharacteristics;
+    InputDeviceCharacteristics rightControllerCharacteristics;
+    InputDeviceCharacteristics deviceCharacteristics;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ////Gets input from face button
+        //targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        //if (primaryButtonValue)
+        //{
+        //    Debug.Log("Pressing primary button.");
+        //}
+
+        ////Gets input from triggers
+        //targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        //if (triggerValue > 0.1f)
+        //{
+        //    Debug.Log("Pressing trigger: " + triggerValue);
+        //}
+
+        ////Gets input from joysticks/touchpads
+        //targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        //if(primary2DAxisValue != Vector2.zero)
+        //{
+        //    Debug.Log("Primary touchpad: " + primary2DAxisValue);
+        //}
+
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        } else
+        {
+            if (showController)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedController.SetActive(true);
+            }
+            else
+            {
+                spawnedHandModel.SetActive(true);
+                spawnedController.SetActive(false);
+                UpdateHandAnimaton();
+            }
+        }
+
+        
+        }
+
+    void UpdateHandAnimaton()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        //leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        //rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        //deviceCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + ", " + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not find model of controller.");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+    }
+}
